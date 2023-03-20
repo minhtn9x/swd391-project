18f25d1070c7ccda6da14dcd56f529defc3f2171
NBMinh63343 <100172707+NBMinh63343@users.noreply.github.com>
2023-03-20 12:55:36 +0700

    thêm validation vào add user

--- src/java/com/dma/controllers/user/AddUserController.java
+++ src/java/com/dma/controllers/user/AddUserController.java
@@ -55,7 +55,69 @@ public class AddUserController extends HttpServlet {
                 Date birthday = Date.valueOf(inputDate);
                 RegisterUserErrorDTO inputUserInfo = new RegisterUserErrorDTO(userID, fullName,
                         address, birthday, phoneNumber, email);
-                
+                if (!dao.isDeleted(userID)) {
+                    if (!dao.checkUserExisted(userID)) {
+                        if (!Validation.isValidUserID(userID)) {
+                            foundError = true;
+                            userError.setUserID("5 - 20 letters. At least one uppercase letter, four numbers");
+                            LOGGER.error("User ID: " + userError.getUserID());
+                        }
+                        if (fullName.trim().length() < 2 || fullName.trim().length() > 30) {
+                            foundError = true;
+                            userError.setFullName("Full Name requires 2 to 30 characters");
+                            LOGGER.error("Full Name: " + userError.getFullName());
+                        }
+                        if (!Validation.isValidPassword(password)) {
+                            foundError = true;
+                            userError.setPassword("Minimum three characters. At least one uppercase letter, one lowercase letter and one number");
+                            LOGGER.error("Password: " + userError.getPassword());
+                        }
+                        if (!password.equals(confirmPassword)) {
+                            foundError = true;
+                            userError.setConfirmPassword("Please confirm your password!");
+                            LOGGER.error("Confirm password: " + userError.getConfirmPassword());
+                        }
+                        if (birthday == null) {
+                            foundError = true;
+                            userError.setErrorMessage("Invalid Date of Birth");
+                            LOGGER.error("Date of Birth: " + userError.getBirthday());
+                        }
+                        if (!phoneNumber.isEmpty()) {
+                            if (!Validation.isValidPhoneNumber(phoneNumber.trim())) {
+                                foundError = true;
+                                userError.setPhoneNumber("Phone number requires 10 numbers");
+                                LOGGER.error("Phone Number: " + userError.getPhoneNumber());
+                            }
+                        }
+                        if (!Validation.isValidEmail(email.toLowerCase().trim())) {
+                            foundError = true;
+                            userError.setEmail("Invalid email pattern");
+                            LOGGER.error("Email: " + userError.getEmail());
+                        } else if (dao.checkEmailExisted(email)) {
+                            foundError = true;
+                            userError.setEmail("This email has been registered");
+                            LOGGER.error("Email: " + userError.getEmail());
+                        }
+                        if (foundError) {
+                            request.setAttribute("USER_ERROR", userError);
+                            request.setAttribute("USER_INFO", inputUserInfo);
+                            LOGGER.info("Registered unsuccessfully");
+                        } else {
+                            boolean checkRegister = dao.createUser(new UserDTO(userID, fullName, "US", password,
+                                    address, birthday, phoneNumber, email, false));
+                            if (checkRegister) {
+                                url = SUCCESS;
+                                request.setAttribute("STATUS", "success");
+                                LOGGER.info("Registered successfully");
+                            }
+                        }
+                    } else {
+                        request.setAttribute("DUPLICATE", "User ID has existed!");
+                        request.setAttribute("USER_INFO", inputUserInfo);
+                    }
+                } else {
+                    request.setAttribute("DELETED_USER", "This user ID had been deleted. Contact us for more infomation!");
+                }
             }
         } catch (Exception e) {
             LOGGER.error(e);
